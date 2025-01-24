afb394d4baaf81013c0372013f732d8373f5b224
erensezen <erenkaansez17@gmail.com>
2025-01-24 15:12:07 +0300

    ProductListButtonUpdate

---
+++ JoygameInventory/Data/Entities/Servers.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoygameInventory.Data.Entities
+{
+    public class Servers
+    {
+            public int ServerId { get; set; }
+            public string ServerName { get; set; }
+            public string IPAddress { get; set; }
+            public string MACAddress { get; set; }
+            public string OperatingSystem { get; set; }
+            public string CPU { get; set; }
+            public int RAM { get; set; }
+            public int Storage { get; set; }
+            public string Status { get; set; }
+            public string Location { get; set; }
+            public DateTime DateInstalled { get; set; }
+            public string AssignedTo { get; set; }
+            public string HostName { get; set; }
+            public string SerialNumber { get; set; }
+            public string NetworkInterface { get; set; }
+            public string PowerStatus { get; set; }
+            public DateTime? LastRebootDate { get; set; }
+            public string BackupStatus { get; set; }
+        
+
+
+
+
+
+    }
+}
