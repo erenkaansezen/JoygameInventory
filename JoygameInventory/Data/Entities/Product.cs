5eebaadb2877159a1f4cf853c4914d252f73c9e8
eren sezen <erenkaansez17@gmail.com>
2025-02-19 01:03:11 +0300

    ClientSideFiltring

--- JoygameInventory/Data/Entities/Product.cs
+++ JoygameInventory/Data/Entities/Product.cs
@@ -32,6 +32,7 @@ namespace JoygameInventory.Data.Entities
 
         public ICollection<InventoryAssigment> InventoryAssigments { get; set; }
         public ICollection<AssigmentHistory> AssigmentHistorys { get; set; }
+
         public ICollection<Maintenance> Maintenances { get; set; } = new List<Maintenance>(); // Servisler koleksiyonu
         public ICollection<MaintenanceHistory> MaintenanceHistory { get; set; } = new List<MaintenanceHistory>(); // Servisler koleksiyonu
 
